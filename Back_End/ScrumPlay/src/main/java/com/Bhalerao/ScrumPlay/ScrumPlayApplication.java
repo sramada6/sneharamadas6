d490404233ced12b95fbb6e85ff05587a348bb6b
avashis9 <avashis9@asu.edu>
2023-10-25 15:42:33 -0700

    Spring Initialisation completed

---
+++ Back_End/ScrumPlay/src/main/java/com/Bhalerao/ScrumPlay/ScrumPlayApplication.java
@@ -0,0 +1,13 @@
+package com.Bhalerao.ScrumPlay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumPlayApplication.class, args);
+	}
+
+}
