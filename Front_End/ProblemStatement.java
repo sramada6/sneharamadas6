016cf3609d83925bafdcf201364c93b97ae733da
mnakawe <mnakawe@asu.edu>
2023-10-26 22:58:52 -0700

    US#011/created product backlog page with user stories and its details

--- Front_End/ProblemStatement.java
+++ Front_End/ProblemStatement.java
@@ -1,14 +1,15 @@
 import java.awt.EventQueue;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
 
 public class ProblemStatement extends JFrame {
     private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+    private JPanel contentPane;
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
@@ -34,20 +35,31 @@ public class ProblemStatement extends JFrame {
         JButton btnNewButton = new JButton("Start Sprint Planning");
         btnNewButton.setBounds(244, 236, 200, 30);
         contentPane.add(btnNewButton);
-        
+
         JLabel lblNewLabel = new JLabel("Your Problem Statement:");
         lblNewLabel.setBounds(6, 6, 200, 20);
         contentPane.add(lblNewLabel);
-        
+
         JLabel lblNewLabel_1 = new JLabel("New label");
         lblNewLabel_1.setText("<html>Build a Scrum Simulator and Training Tool to help users understand<br> the Agile methodology.</html>");
         lblNewLabel_1.setBounds(6, 29, 428, 46);
         contentPane.add(lblNewLabel_1);
 
+        JLabel lblNewLabel_2 = new JLabel("Notes Regarding Problem Statement:");
+        lblNewLabel_2.setBounds(6, 101, 235, 16);
+        contentPane.add(lblNewLabel_2);
+
+        JTextArea textArea = new JTextArea();
+        textArea.setBounds(6, 123, 428, 51);
+        contentPane.add(textArea);
+
+        JButton btnNewButton_1 = new JButton("Add New Problem Statement ");
+        btnNewButton_1.setBounds(6, 237, 215, 29);
+        contentPane.add(btnNewButton_1);
+
         btnNewButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // Create and show a new JFrame when the button is clicked
-            	ProductBacklog newFrame = new ProductBacklog();
+                ProductBacklog newFrame = new ProductBacklog();
                 newFrame.setVisible(true);
             }
         });
--- Front_End/ProductBacklog.java
+++ Front_End/ProductBacklog.java
@@ -1,21 +1,86 @@
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProductBacklog extends JFrame {
-    private static final long serialVersionUID = 3634750089181129308L;
+    private JList<String> userList;
+    private DefaultListModel<String> listModel;
+    private JPanel detailPanel;
+    private Map<String, String> userStoryDescriptions;
+    private JTextField userStoryPointsField;
+    private JComboBox<String> playerDropdown;
 
-	public ProductBacklog() {
+    public ProductBacklog() {
         setTitle("Product Backlog");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setBounds(100, 100, 450, 300);
+        setBounds(100, 100, 800, 500);
 
-        // Design the UI for the new page here...
-        
-        // For example, you can add components to the new frame as needed
-        JLabel label = new JLabel("");
-        label.setVerticalAlignment(SwingConstants.TOP);
-        label.setBounds(6, 6, 200, 20);
-        getContentPane().add(label);
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        setContentPane(mainPanel);
+
+        listModel = new DefaultListModel<>();
+        listModel.addElement("US#001/Create Landing Page");
+        listModel.addElement("US#002/Follow up for Scrum Intro");
+        listModel.addElement("US#003/Create Database");
+        listModel.addElement("US#004/Design game configuration page");
+        listModel.addElement("US#005/Display previous game scores and sprint history");
+
+        userList = new JList<>(listModel);
+        userList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        JScrollPane listScrollPane = new JScrollPane(userList);
+        listScrollPane.setPreferredSize(new Dimension(200, 0));
+
+        detailPanel = new JPanel(new BorderLayout());
+        mainPanel.add(listScrollPane, BorderLayout.WEST);
+        mainPanel.add(detailPanel, BorderLayout.CENTER);
+
+        userList.addListSelectionListener(e -> {
+            if (!e.getValueIsAdjusting()) {
+                String selectedUserStory = userList.getSelectedValue();
+                showUserStoryDetails(selectedUserStory);
+            }
+        });
+
+        userStoryDescriptions = new HashMap<>();
+        userStoryDescriptions.put("US#001/Create Landing Page", "This is the description for Create Landing Page.");
+        userStoryDescriptions.put("US#002/Follow up for Scrum Intro", "This is the description for Follow up for Scrum Intro.");
+        userStoryDescriptions.put("US#003/Create Database", "This is the description for Create Database.");
+        userStoryDescriptions.put("US#004/Design game configuration page", "This is the description for Design game configuration page.");
+        userStoryDescriptions.put("US#005/Display previous game scores and sprint history", "This is the description for Display previous game scores and sprint history.");
+    }
+
+    private void showUserStoryDetails(String userStory) {
+        detailPanel.removeAll();
+
+        JPanel userStoryInfoPanel = new JPanel(new GridLayout(1, 2));
+        JPanel pointsDescriptionPanel = new JPanel(new BorderLayout());
+
+        JPanel assignmentPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        playerDropdown = new JComboBox<>(new String[]{"Player 1", "Player 2", "Player 3", "Player 4", "Player 5"});
+        assignmentPanel.add(new JLabel("Assign to:"));
+        assignmentPanel.add(playerDropdown);
+
+        userStoryPointsField = new JTextField(10);
+        JPanel pointsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        pointsPanel.add(new JLabel("User Story Points: "));
+        pointsPanel.add(userStoryPointsField);
+
+        JLabel userStoryLabel = new JLabel("User Story: " + userStory);
+
+        String description = userStoryDescriptions.get(userStory);
+        JLabel userStoryDescription = new JLabel("User Story Description:" + description);
+
+        userStoryInfoPanel.add(userStoryLabel);
+        userStoryInfoPanel.add(assignmentPanel);
+
+        pointsDescriptionPanel.add(pointsPanel, BorderLayout.WEST);
+        pointsDescriptionPanel.add(userStoryDescription, BorderLayout.CENTER);
+
+        detailPanel.add(userStoryInfoPanel, BorderLayout.NORTH);
+        detailPanel.add(pointsDescriptionPanel, BorderLayout.CENTER);
+        detailPanel.revalidate();
+        detailPanel.repaint();
     }
 }
