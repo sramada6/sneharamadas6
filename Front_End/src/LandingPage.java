e9b551bf42c3e991c61e69b3765f5a9294d3e73b
djani4 <djani4@asu.edu>
2023-10-30 21:47:02 -0700

    Taiga Task#34 for US#001/Create-Landing-Page

--- Front_End/src/LandingPage.java
+++ Front_End/src/LandingPage.java
@@ -1,10 +1,21 @@
 import java.awt.EventQueue;
+import java.awt.Image;
 
 import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.BoxLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.ImageIcon;
+import java.awt.Font;
 
 public class LandingPage {
 
 	private JFrame frame;
+	private JLabel logo;
+	private JLabel title;
+	private JLabel Description;
 
 	/**
 	 * Launch the application.
@@ -34,8 +45,29 @@ public class LandingPage {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 30));
+		frame.getContentPane().setForeground(new Color(128, 0, 0));
+		frame.getContentPane().setBackground(new Color(0, 206, 209));
+		frame.getContentPane().setLayout(null);
+		
+		logo = new JLabel("");
+		Image img = new ImageIcon(this.getClass().getResource("/ScrumPlay Logo.jpg")).getImage();
+		logo.setIcon(new ImageIcon(img));
+		logo.setBounds(10, 24, 100, 100);
+		frame.getContentPane().add(logo);
+		
+		title = new JLabel("ScrumPlay: A Scrum Game Experience");
+		title.setForeground(new Color(128, 0, 0));
+		title.setFont(new Font("Tahoma", Font.PLAIN, 45));
+		title.setBounds(119, 24, 770, 100);
+		frame.getContentPane().add(title);
+		
+		Description = new JLabel("Lets Start With a Quick Lesson About Scrum! Click Read More for a Detailed Description...");
+		Description.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		Description.setBounds(10, 135, 852, 43);
+		frame.getContentPane().add(Description);
+		frame.setBounds(100, 100, 878, 374);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-
 }
+ 
