1c9103a6136e7a4f9ae18788dbec86c44c492564
avashis9 <avashis9@asu.edu>
2023-11-01 16:26:54 -0700

    changed add-player endpoint to add-gameConfig

--- Back_End/ScrumPlay/src/main/java/com/Bhalerao/ScrumPlay/controller/PlayerController.java
+++ Back_End/ScrumPlay/src/main/java/com/Bhalerao/ScrumPlay/controller/PlayerController.java
@@ -1,7 +1,9 @@
 package com.Bhalerao.ScrumPlay.controller;
 
 import com.Bhalerao.ScrumPlay.Dto.PlayerDto;
+import com.Bhalerao.ScrumPlay.Dto.SprintDto;
 import com.Bhalerao.ScrumPlay.service.PlayerService;
+import com.Bhalerao.ScrumPlay.service.SprintService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,27 +12,53 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class PlayerController {
+    private static final Logger log = LoggerFactory.getLogger(PlayerController.class);
+
     private PlayerService playerService;
+    private SprintService sprintService;
 
     @Autowired
-    PlayerController(PlayerService playerService)
-    {
+    public PlayerController(PlayerService playerService, SprintService sprintService) {
         this.playerService = playerService;
+        this.sprintService = sprintService;
     }
 
-    @PostMapping("/add-player")
-    public ResponseEntity<String> addPlayer(@RequestBody PlayerDto playerDto) {
+    @PostMapping("/add-gameConfig")
+    public ResponseEntity<String> addPlayer(@RequestBody Map<String, Object> requestData) {
         try {
-            System.out.print(playerDto);
+            String playerName = (String) requestData.get("playerName");
+            String playerRole = (String) requestData.get("playerRole");
+
+            PlayerDto playerDto = PlayerDto.builder()
+                    .playerName(playerName)
+                    .playerRole(playerRole)
+                    .build();
+
             playerService.savePlayer(playerDto);
-            return ResponseEntity.ok("Player added successfully");
+
+            int teamSize = (int) requestData.get("teamSize");
+            int sprintLength = (int) requestData.get("sprintLength");
+            Float scrumCallLength = ((Integer) requestData.get("scrumCallLength")).floatValue();
+
+            SprintDto sprintDto = SprintDto.builder()
+                    .teamSize(teamSize)
+                    .sprintLength(sprintLength)
+                    .scrumCallLength(scrumCallLength)
+                    .build();
+            sprintService.saveSprint(sprintDto);
+
+            return ResponseEntity.ok("Player and Sprint added successfully");
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error adding player");
+            log.error("Error adding player and sprint", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error adding player and sprint");
         }
     }
 }
