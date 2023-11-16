bae4bbdbe4d23d8bb68113320f1648359c7ca026
Namita Bhalerao <nbhaler2@asu.edu>
2023-11-15 21:55:24 -0700

    Taiga task#60 for US#015/add functionality to provide scrum update and choose member from drop down

--- Front_End/src/ScrumPlay_FrontEnd/ScrumBoard.java
+++ Front_End/src/ScrumPlay_FrontEnd/ScrumBoard.java
@@ -15,26 +15,40 @@ public class ScrumBoard extends JFrame {
         // Initialize user data
         userData = new HashMap<>();
 
-        // Create sample user data
         Map<String, String> user1Data = new HashMap<>();
-        user1Data.put("UserStory1", "To Do");
-        user1Data.put("UserStory2", "In Progress");
-        user1Data.put("UserStory3", "Done");
+        user1Data.put("Implement User Authentication", "To Do");
+        user1Data.put("Design Scrum Board UI", "In Progress");
+        user1Data.put("Implement Task Management", "Done");
 
         Map<String, String> user2Data = new HashMap<>();
-        user2Data.put("UserStory4", "To Do");
-        user2Data.put("UserStory5", "Done");
+        user2Data.put("Create Sprint Planning Module", "To Do");
+        user2Data.put("Test Scrum Board Functionality", "Done");
 
-        userData.put("User1", user1Data);
-        userData.put("User2", user2Data);
+        Map<String, String> user3Data = new HashMap<>();
+        user3Data.put("Refactor Codebase", "In Progress");
+        user3Data.put("Optimize Database Queries", "Done");
+
+        Map<String, String> user4Data = new HashMap<>();
+        user4Data.put("Implement Notifications", "To Do");
+        user4Data.put("Conduct Usability Testing", "In Progress");
+
+        Map<String, String> user5Data = new HashMap<>();
+        user5Data.put("Write User Documentation", "To Do");
+        user5Data.put("Prepare Release Notes", "Done");
+
+        userData.put("JohnDoe", user1Data);
+        userData.put("JaneSmith", user2Data);
+        userData.put("RobertJohnson", user3Data);
+        userData.put("EmilyDavis", user4Data);
+        userData.put("MichaelBrown", user5Data);
 
         // Set up the main frame
-        setTitle("JIRA Board");
+        setTitle("Scrum Board");
         setSize(1200, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
+        getContentPane().setBackground(new Color(240, 240, 240)); // Set background color
 
-        // Create user dropdown
         userDropdown = new JComboBox<>(userData.keySet().toArray(new String[0]));
         userDropdown.addActionListener(new ActionListener() {
             @Override
@@ -43,14 +57,22 @@ public class ScrumBoard extends JFrame {
             }
         });
 
-        // Create the board panel
         boardPanel = createBoardPanel();
         add(userDropdown, BorderLayout.NORTH);
         add(boardPanel, BorderLayout.CENTER);
 
-        // Create the update panel
         JPanel updatePanel = createUpdatePanel();
         add(updatePanel, BorderLayout.SOUTH);
+
+        // Create the end Scrum call button
+        JButton endScrumButton = new JButton("End Scrum Call");
+        endScrumButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                endScrumCall();
+            }
+        });
+        add(endScrumButton, BorderLayout.PAGE_END);
 
         // Initial board update
         updateBoard();
@@ -62,7 +84,8 @@ public class ScrumBoard extends JFrame {
         for (String lane : new String[]{"To Do", "In Progress", "Done"}) {
             JPanel lanePanel = new JPanel();
             lanePanel.setLayout(new BoxLayout(lanePanel, BoxLayout.Y_AXIS));
-            lanePanel.setBorder(BorderFactory.createTitledBorder(lane));
+            lanePanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK, 2), lane));
+            lanePanel.setBackground(new Color(255, 255, 224)); // Set lane background color
 
             boardPanel.add(lanePanel);
         }
@@ -90,27 +113,30 @@ public class ScrumBoard extends JFrame {
         JPanel cardPanel = new JPanel();
         cardPanel.setLayout(new BoxLayout(cardPanel, BoxLayout.Y_AXIS));
         cardPanel.setBorder(BorderFactory.createEtchedBorder());
+        cardPanel.setBackground(new Color(255, 250, 250)); // Set card background color
+        cardPanel.setPreferredSize(new Dimension(800, 200)); // Set fixed size
 
-        // User story label
         JLabel userStoryLabel = new JLabel(userStory);
-        userStoryLabel.setFont(new Font("Arial", Font.BOLD, 14));
+        userStoryLabel.setFont(new Font("Arial", Font.BOLD, 12)); // Reduce font size
         userStoryLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         cardPanel.add(userStoryLabel);
 
-        // Flip card button
-        JButton flipButton = new JButton("Flip Card");
+        cardPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        JButton flipButton = new JButton("Update Status");
         flipButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         flipButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Show update in the update area
-                updateArea.setText(getUpdateForUserStory(userStory));
+                // Show daily status input dialog
+                showStatusInputDialog(userStory);
             }
         });
         cardPanel.add(flipButton);
 
         return cardPanel;
     }
+
 
     private void addToLanePanel(String status, JPanel cardPanel) {
         Component[] lanePanels = boardPanel.getComponents();
@@ -141,6 +167,7 @@ public class ScrumBoard extends JFrame {
     private JPanel createUpdatePanel() {
         JPanel updatePanel = new JPanel();
         updatePanel.setLayout(new BorderLayout());
+        updatePanel.setBackground(new Color(240, 240, 240)); // Set background color
 
         // Update area
         updateArea = new JTextArea();
@@ -152,9 +179,28 @@ public class ScrumBoard extends JFrame {
     }
 
     private String getUpdateForUserStory(String userStory) {
-        // In a real application, you would fetch the update from the database or some other source
-        // For simplicity, using dummy data here
+
         return "Update for " + userStory + ": Today's progress...";
+    }
+
+    private void showStatusInputDialog(String userStory) {
+        JTextArea statusInput = new JTextArea();
+        statusInput.setLineWrap(true);
+        statusInput.setWrapStyleWord(true);
+
+        JScrollPane scrollPane = new JScrollPane(statusInput);
+
+        int result = JOptionPane.showConfirmDialog(this, scrollPane, "Enter Daily Status for " + userStory,
+                JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+        if (result == JOptionPane.OK_OPTION) {
+            String dailyStatus = statusInput.getText();
+            updateArea.setText("Daily Status for " + userStory + ":\n" + dailyStatus);
+        }
+    }
+
+    private void endScrumCall() {
+        JOptionPane.showMessageDialog(this, "Scrum Call Ended!", "Scrum Ended", JOptionPane.INFORMATION_MESSAGE);
     }
 
     public static void main(String[] args) {
