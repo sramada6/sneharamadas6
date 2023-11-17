921f1edce98544e0b0c903157452517d8065e156
mnakawe <mnakawe@asu.edu>
2023-11-17 11:58:48 -0700

    Taiga Task#59 for US#009/Added Unit Test Cases for BackEnd

--- Back_End/ScrumPlay/src/main/java/com/Bhalerao/ScrumPlay/model/Player.java
+++ Back_End/ScrumPlay/src/main/java/com/Bhalerao/ScrumPlay/model/Player.java
@@ -1,12 +1,13 @@
 package com.Bhalerao.ScrumPlay.model;
 
 import jakarta.persistence.*;
+//import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//import javax.persistence.*;
+
 
 @Data
 @NoArgsConstructor
