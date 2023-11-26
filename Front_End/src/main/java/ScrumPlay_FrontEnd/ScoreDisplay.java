f5deabb9ff5c6f09b2be4aa64ac0cf505926b822
Namita Bhalerao <nbhaler2@asu.edu>
2023-11-25 18:36:21 -0700

    Taiga task#80 for US#020/included functionality to fetch json data from REST endpoint

--- Front_End/src/main/java/ScrumPlay_FrontEnd/ScoreDisplay.java
+++ Front_End/src/main/java/ScrumPlay_FrontEnd/ScoreDisplay.java
@@ -1,7 +1,13 @@
 package ScrumPlay_FrontEnd;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -14,8 +20,8 @@ public class ScoreDisplay extends JFrame {
         setSize(400, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Fetch and update scores from JSON (replace this with your actual logic)
-        updateScoresFromJson("{ \"player1\": 5, \"player2\": 7 }");
+        // Fetch and update scores from JSON (replace the URL with your actual REST endpoint)
+        updateScoresFromRestEndpoint("http://localhost:8080/add-player-score");
 
         // Create a table model with two columns: Player and Score
         String[] columnNames = {"Player", "Score"};
@@ -43,16 +49,28 @@ public class ScoreDisplay extends JFrame {
         tableModel.addRow(new Object[]{"Player 2", scorePlayer2});
     }
 
-    // Method to fetch scores from JSON and update the table
-    private void updateScoresFromJson(String json) {
+    // Method to fetch scores from a REST endpoint and update the table
+    private void updateScoresFromRestEndpoint(String restEndpointUrl) {
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(json);
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(restEndpointUrl))
+                    .build();
 
-            int scorePlayer1 = jsonNode.get("player1").asInt();
-            int scorePlayer2 = jsonNode.get("player2").asInt();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
 
-            updateScores(scorePlayer1, scorePlayer2);
+            if (response.statusCode() == 200) {
+                String jsonResponse = response.body();
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode jsonNode = objectMapper.readTree(jsonResponse);
+
+                int scorePlayer1 = jsonNode.get("player1").asInt();
+                int scorePlayer2 = jsonNode.get("player2").asInt();
+
+                updateScores(scorePlayer1, scorePlayer2);
+            } else {
+                System.out.println("Failed to fetch data from the REST endpoint. Status code: " + response.statusCode());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
